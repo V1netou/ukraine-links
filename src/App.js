90392f48720854d502bf2c9f7e5a1b62f1c3f369
Samko <samuel.fano@gmail.com>
2022-03-03 19:29:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {Fragment} from 'react';
 import LinkBtn from './components/LinkBtn.jsx';
 import { faBed } from '@fortawesome/free-solid-svg-icons'
+import { fabriefcase } from '@fortawesome/free-solid-svg-icons'
+import { faBed } from '@fortawesome/free-solid-svg-icons'
+import { faBed } from '@fortawesome/free-solid-svg-icons'
+import { faBed } from '@fortawesome/free-solid-svg-icons'
 import GlobalStyle from './theme/globalStyle';
 import styled from 'styled-components';
 import Container from './components/Container';
@@ -11,7 +15,8 @@ function App() {
     <Fragment>
       <GlobalStyle/>
       <Container>
-        <LinkBtn text="Проживання - Ubytovanie" link="https://fontawesome.com/icons/bed?s=light" icon={faBed}></LinkBtn>
+        <LinkBtn text="Проживання - Ubytovanie" link="https://forms.office.com/r/UDAvkvqMcv" icon={faBed}></LinkBtn>
+        <LinkBtn text="Проживання - Ubytovanie" link="https://forms.office.com/r/48SQSTx00S " icon={fabriefcase}></LinkBtn>
       </Container>
     </Fragment>
   );
